6ccc18ed0a54f40cf320daa40d70aaa5f36aaf2e
[MasterS007] <[munirasirajum13@gmail.com]>
2020-04-05 00:30:12 +0600

    Receptionist Class added

---
+++ WindowsFormsAppPocketDoctorSolution/WindowsFormsAppPocketDoctorProject/Classes/Receptionist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppPocketDoctorProject.Classes
+{
+    internal class Receptionist:User
+    {
+
+        internal Receptionist(string userName, string password, string mobileNumber, string role) : base(userName, password, mobileNumber, role)
+        { }
+
+    }
+}
