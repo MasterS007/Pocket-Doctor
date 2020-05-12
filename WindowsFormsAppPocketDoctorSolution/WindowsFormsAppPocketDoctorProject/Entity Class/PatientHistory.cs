6d2d75b79b40fc3dd7044fb323f645e313c35d74
[MasterS007] <[munirasirajum13@gmail.com]>
2020-05-13 01:20:38 +0600

    PatientHistory class added

---
+++ WindowsFormsAppPocketDoctorSolution/WindowsFormsAppPocketDoctorProject/Entity Class/PatientHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppPocketDoctorProject.Entity_Class
+{
+    class PatientHistory
+    {
+       private string prescription { get; set; }
+       private int hist_id { get; set; }
+       private int rep_id { get; set; }
+       private string p_id { get; set; }
+
+    }
+}
